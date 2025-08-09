5c9778f8fd9f0f08716e10a7ad324ae7d93b69f2
sincerajn <sincerajn@outlook.com>
2025-08-09 19:58:29 +0800

    ~

---
+++ src/components/Markdown.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { parse } from 'marked'
+defineProps<{ markdown: string }>()
+</script>
+
+<template>
+  <article v-html="parse(markdown)" />
+</template>
+
+<style scoped>
+article {
+  line-height: 1.5;
+}
+
+article:deep(h1) {
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: #27a6ba;
+}
+
+article:deep(h2) {
+  font-weight: bold;
+}
+
+article:deep(p) {
+  margin-block-end: 0.5rem;
+}
+
+article:deep(ul, ol) {
+  padding-inline-start: 2rem;
+}
+</style>
--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,56 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import introduce from '@/assets/contents/introduce.md?raw'
+import Markdown from '@/components/Markdown.vue'
+</script>
 
 <template>
-  <article>
-    <h1>CustomizeWeapon</h1>
-    <p>CustomizeWeapon 为全面定制武器提供框架级支持，并内置原版及 DLC 武器的原生支持。</p>
-    <h2>注意</h2>
-    <p>本项目正在开发完善阶段，任何模块效果皆<b>不代表</b>最终强度。</p>
-    <p>动态贴图暂只适配：</p>
-    <ul>
-      <li>突击步枪</li>
-      <li>栓动步枪</li>
-      <li>狙击步枪</li>
-      <li>轻机枪</li>
-      <li>链式霰弹枪</li>
-    </ul>
-    <p>且上述武器也皆未完全适配全部模块。</p>
-    <h2>参与其中</h2>
-    <ul>
-      <li>QQ 群：710882819</li>
-      <li>代码仓库：<a href="https://github.com/realloon/CustomizeWeaponFramework">Github</a></li>
-      <li>抢先体验：<a
-          href="https://steamcommunity.com/sharedfiles/filedetails/?id=3537051503">创意工坊（非公开发布）</a>，若遇到任何问题，请与我联系！</li>
-    </ul>
-    <p>恳请您<b>不要直接传播</b>上述创意工坊地址，至少，请直接发送本页面。</p>
-    <h2>许可</h2>
-    <p>CustomizeWeapon 项目采用<b style="color: red;">双重协议</b>许可，请须知：</p>
-    <ul>
-      <li>所有 C# 代码为 MIT 协议开源。可自由学习、修改、分发或用于其他项目；</li>
-      <li>所有美术资源（包括且不限于封面、贴图、图标）均为<strong>版权所有</strong>，保留所有权利（All Rights
-        Reserved）。在未经我明确书面许可的情况下，<strong>禁止</strong>以任何形式在项目中使用、复制、修改或重新分发这些资源。</li>
-    </ul>
-  </article>
+  <Markdown :markdown="introduce" />
 </template>
-
-<style scoped>
-article {
-  line-height: 1.5
-}
-
-h1 {
-  font-weight: bold;
-  font-size: 1.5rem;
-  color: #1766c1;
-  text-shadow: 2px 2px 2px #00000016;
-}
-
-h2 {
-  font-weight: bold;
-  margin-block-start: 1rem;
-}
-
-p {
-  margin-block: 0.5rem;
-}
-</style>
