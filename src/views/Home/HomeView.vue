8b0dbde652ebc80c5bb38e9d8abd3e470bee9146
sincerajn <sincerajn@outlook.com>
2025-08-12 21:07:50 +0800

    ~

---
+++ src/views/Home/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import introduce from '@/assets/contents/introduce.md?raw'
+import Markdown from '@/components/Markdown.vue'
+</script>
+
+<template>
+  <Markdown :markdown="introduce" />
+</template>
+
+<style scoped></style>
