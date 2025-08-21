8809dccdbbf03af965433a67f902b19cc20178c2
sincerajn <sincerajn@outlook.com>
2025-08-21 23:16:25 +0800

    ~

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,19 @@ import '@/assets/styles/main.css'
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from 'vue-router/auto-routes'
 import { setupLayouts } from 'virtual:generated-layouts'
+import { createHead } from '@vueuse/head'
 import App from './App.vue'
+
+const app = createApp(App)
 
 const router = createRouter({
   history: createWebHistory(),
   routes: setupLayouts(routes),
 })
 
-const app = createApp(App)
+const head = createHead()
+
 app.use(router)
+app.use(head)
 
 app.mount('#app')
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
 import introduce from '@/contents/introduce.md?raw'
 import Header from './components/Header.vue'
+import { useHead } from '@vueuse/head'
 definePage({ path: '/' })
+
+useHead({
+  meta: [
+    {
+      name: 'description',
+      content:
+        'Customize Weapon mod build-in framework: CustomizeWeaponFramework',
+    },
+  ],
+})
 </script>
 
 <template>
